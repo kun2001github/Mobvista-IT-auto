fb3a6f9143cebc14dbf9ff56ee6048c76ac54d7d
kun2001github <120442804+kun2001github@users.noreply.github.com>
2024-07-21 03:56:11 +0800

    It's time to go to sleep

--- UAC/UAC_level_default.ps1
+++ UAC/UAC_level_default.ps1
@@ -2,6 +2,7 @@
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 5
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Value 1
 
+# 获取UAC状态
 $uacStatus = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
 Write-Host "ConsentPromptBehaviorAdmin: " $uacStatus.ConsentPromptBehaviorAdmin
 Write-Host "PromptOnSecureDesktop: " $uacStatus.PromptOnSecureDesktop
