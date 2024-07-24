1f97326404351d5f780d964b69eed43ab60b2acf
kun2001github <1175553187@qq.com>
2024-07-24 13:55:08 +0800

    移除电源管理的自动脚本，有问题

---
+++ power_supply/test.ps1
@@ -0,0 +1,12 @@
+# 设置电源按钮和盖子行为为不采取任何操作
+
+# 电源按钮行为
+powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 0
+powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 0
+
+# 盖子关闭行为
+powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+
+# 应用更改
+powercfg -SetActive SCHEME_CURRENT
